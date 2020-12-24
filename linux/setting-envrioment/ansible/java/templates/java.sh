35e17a5f30794e74c9b3355fda39f07dac07a507
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-12-24 09:01:39 -0300

    Configurando maven

--- linux/setting-envrioment/ansible/java/templates/java.sh
+++ linux/setting-envrioment/ansible/java/templates/java.sh
@@ -1,3 +1,3 @@
 #PATH AND JAVA_HOME TO AMAZON_CORRETO_JDK11
 export PATH={{java_jdks_folder}}/amazon-correto-jdk11/bin:$PATH
-export JAVA_HOME={{java_jdks_folder}}/amazon-correto-jdk11/bin
+export JAVA_HOME={{java_jdks_folder}}/amazon-correto-jdk11
---
+++ linux/setting-envrioment/ansible/maven/templates/maven.sh
@@ -0,0 +1,2 @@
+#PATH TO APACHE MAVEN
+export PATH={{opt_folder}}/{{apache_maven_folder}}/bin:$PATH
